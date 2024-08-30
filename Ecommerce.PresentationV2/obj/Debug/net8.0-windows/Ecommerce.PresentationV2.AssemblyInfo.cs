74258ad47a81cf2e175baee02f964034d317a505
Menna33566 <mm5955100@gmail.com>
2024-08-30 18:46:24 +0300

    presentation

--- Ecommerce.PresentationV2/obj/Debug/net8.0-windows/Ecommerce.PresentationV2.AssemblyInfo.cs
+++ Ecommerce.PresentationV2/obj/Debug/net8.0-windows/Ecommerce.PresentationV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.PresentationV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5146fd1485f80a2e3bdbfc78562d30365a50edd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbb7fe8f77ee25301ec21acedb7bfac9f79ed5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.PresentationV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.PresentationV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
